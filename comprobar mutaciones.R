909becdb46ec2f524c4da383cdb351aa4567038a
fenris123 <fenris1234@hotmail.com>
2015-05-07 12:04:28 +0200

    actualizacion del script
    Actualizacion del script. Ahora es capaz de analizar varias mutaciones
    mientras estas no afecten a la pauta de lectura

--- comprobar mutaciones.R
+++ comprobar mutaciones.R
@@ -4,57 +4,70 @@
 
 ####### This functions take  FASTA files on a directory and compares against another FASTA file ####
 
-####### WARNING: package "seqinr" it's required  #########
+####### WARNING: package "seqinr" it's required . be sure you have it installed.#########
 
 ####### Remember to insert the variables inside of  " " ####
 
 
 
+library (seqinr)
+
 mutaciones <- function (modelo,directorio){
      
      
-     modelsequence <- read.fasta(modelo)                            ### this read the FASTA used as model to compare, and 
-     clearsequence <- as.vector(unlist(modelsequence))                           ### gives us only the sequence. 
-         
-     
-     
-     muestras <- list.files (directorio, full.names=TRUE)           ### This creates a list with all the files of our directory.
+     modelsequence <- read.fasta(modelo)                               ### this read the FASTA used as model to compare, and 
+     clearsequence <- as.vector(unlist(modelsequence))                 ### gives us only the sequence. 
      
      
      
+     muestras <- list.files (directorio, full.names=TRUE)              ### This creates a list with all the files of our directory.
      
      
-          for (i in 1:length(muestras)){                              ### This select all the files on the directory
-               
-               
-               fastatested <- read.fasta (muestras[i])
-               tested <-as.vector(unlist(fastatested))
-               
-               
-                    for (j in 1:length(clearsequence)){                ### This compares the sequence, checking if there is mutations
+ 
+     
+#### From here we begin the comparation between chains   
+     
           
-                         if (tested[j] != (clearsequence[j])) {
-                             
-                              cat ("Existe una mutacion en la posicion", paste (j), "del archivo", paste (muestras[i]), sep =" ")
-                              
-                              
-                              
-                              if (length(clearsequence)==length(tested)){                      ### this test if the mutation afects or not to the read fase
-                                   
-                                   message ("La mutacion no afecta a la fase de lectura")}
-                                   
-                              else {
-                                   
-                                   message ("La mutacion afecta a la fase de lectura")}
-                                   
-                              break   
-                             
-                              
-                              
-                              }
+for (i in 1:length(muestras)){                                          ### This select all the files on the directory
+    
+     
+     fastatested <- read.fasta (muestras[i])
+     tested <-as.vector(unlist(fastatested))
+          
+     
+     
+          
+     for (j in 1:length(clearsequence)){                               ### This compares the sequence, checking if there is mutations
+               
+          if (tested[j] != clearsequence[j]) {
                     
-                    }   
-     
-          }
+               cat ("Existe una mutacion en la posicion", paste (j), "del archivo", paste (muestras[i]), sep =" ")
+               
+               
+            
+               next15tested <- paste(tested[(j+1):(j+16)], sep="", collapse="")                   ###this check if the mutations afect to the reading frame
+               
+               next15clear  <- paste(clearsequence[(j+1):(j+16)], sep="", collapse="")
+               
+               if (next15tested != next15clear) {
+                    
+                    
+                    message ("Es posible que la mutacion afecte a la pauta de lectura")
+                    break}
+                    
+               else  { 
+                 
+                 message ("la mutacion no parece afectar a la pauta de lectura")  
+            
+            
+            
+                          
+                    
+                     }
+               }
+               
+          }   
           
+     }
+     
 }
